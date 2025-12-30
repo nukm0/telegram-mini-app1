7f0d9c3c95cdb0f437d74ddaf42e9ed1bf90aa17
nukm0 <netankyu@gmail.com>
2025-12-30 07:43:08 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,10 @@
-// Простой Service Worker
+// Простой Service Worker для кэширования
 const CACHE_NAME = 'vape-market-v1';
-const urlsToCache = ['/', '/index.html', '/style.css', '/app.js'];
 
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-    );
+    console.log('Service Worker установлен');
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
-    );
+    event.respondWith(fetch(event.request));
 });
