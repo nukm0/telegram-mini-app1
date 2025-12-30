433f499bdda30a71d6da39115ed3c63549df5e33
nukm0 <netankyu@gmail.com>
2025-12-30 08:42:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ const CONFIG = {
     // =================== ПРИЛОЖЕНИЕ ===================
     APP_NAME: 'Vape Market',
     VERSION: '1.0.0',
-    API_URL: 'https://vape-tg-working.vercel.app/api',
+    API_URL: '',
     
     // =================== ОГРАНИЧЕНИЯ ===================
     MAX_IMAGES_PER_AD: 3,
